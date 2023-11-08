614e985d2e9e63adfba36e45c33d06e9139c3a45
ash masala <annavalentina2006.am@gmail.com>
2023-11-08 11:15:20 +0100

    Opdracht01

---
+++ H01/opdracht01/index.php
@@ -0,0 +1,31 @@
+<?php
+$a = "Ik heb niet gefaald";
+$b = "die niet werken";
+$c = 10000;
+
+echo $a . ". Ik heb alleen " . $c . " manieren gevonden " . $b;
+?>
+
+<?php
+$a = "die geen fout";
+$b = "en";
+$c = "niet.";
+
+echo "<br> Mensen " . $a . "en maken, werk" . $b . " " . $c;
+?>
+
+<?php
+$a = "ry";
+$b = ", but";
+$c = "fail";
+
+echo "<br> T" . $a . " and " . $c . $b . " never fail to try.";
+?>
+
+<?php
+$a = "\"omgaan";
+$b = "met";
+$c = "teleu";
+
+echo "<br> De cursus " . $a . $b . $c . "rstellingen\" kan vanavond helaas niet doorgaan.";
+?>
