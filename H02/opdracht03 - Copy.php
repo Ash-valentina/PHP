609ab9cef73f1d9091a4956478f0819fd8b8e067
ash masala <annavalentina2006.am@gmail.com>
2023-11-09 11:22:50 +0100

    opdracht03 H02

---
+++ H02/opdracht03 - Copy.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    for($i = 35; $i >= 25; $i--){
+        echo "hoppelepee";
+    }
+    ?>
+</body>
+</html>
